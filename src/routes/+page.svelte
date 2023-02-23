148f2c5ae37485bb5588b59fb5978fced17d6bbb
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-02-24 11:40:36 +1300

    feat: added bruh sound on click

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,18 +11,22 @@
     let count = 0;
     let count2 = 1;
     let count3 = 1;
+    let audio;
     function incrementCount(){
         count++;
     }
     function incrementPadding(){
+        
         let pad = document.getElementById("paddings");
         count2++;
         count3 += 0.1;
         pad.style.padding = (count2)+"vw";
         pad.style.fontSize = (count3)+"em";
+        audio.play();
     }
 </script>
 
 <button on:click={incrementCount} id="paddings" on:click={incrementPadding}>
     Clicked {count} {count === 1 ? 'time' : "times"}
 </button>
+<audio src="https://cdn.freesound.org/previews/506/506582_11033252-lq.mp3" bind:this={audio}></audio>
