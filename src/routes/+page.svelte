aea251629d7207362f8bf19b6f61d6b561469482
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-02-24 11:30:26 +1300

    feat: button enlarges on every click

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p></p>
+<style>
+    button {
+        background-color: aquamarine;
+        border: none;
+        margin: 2vw;
+        padding: 3vw;
+    }
+</style>
+
+<script>
+    let count = 0;
+    let count2 = 1;
+    let count3 = 1;
+    function incrementCount(){
+        count++;
+    }
+    function incrementPadding(){
+        let pad = document.getElementById("paddings");
+        count2++;
+        count3 += 0.1;
+        pad.style.padding = (count2)+"vw";
+        pad.style.fontSize = (count3)+"em";
+    }
+</script>
+
+<button on:click={incrementCount} id="paddings" on:click={incrementPadding}>
+    Clicked {count} {count === 1 ? 'time' : "times"}
+</button>
