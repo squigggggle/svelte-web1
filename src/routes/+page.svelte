d384944bfbef207fc8fc5c3ec14a2c0d5e32e316
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-03-14 09:54:22 +1300

    chore: layout changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <div class="wrapper">
     <div>sidebar1</div>
-    <div>Two</div>
-    <div class="box3">
-      <p>Three</p>
-      <div class="nest">I have</div>
-      <div class="nest">three</div>
-      <div class="nest">boxes</div>
-    </div>
-    <div>Four</div>
-    <div>sidebar2</div>
-  </div>
+  	<div>Two</div>
+  	<div class="box3">
+    	<p>Three</p>
+    	<div class="nest">I have</div>
+    	<div class="nest">three</div>
+    	<div class="nest">boxes</div>
+ 	</div>
+  	<div>Four</div>
+  	<div>sidebar2</div>
+</div>
