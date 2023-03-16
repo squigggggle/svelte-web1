b8f5a553b9cef789b9053e87f451cfe52ba86a2d
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-03-17 11:42:28 +1300

    feat: improved grid layout, scoped grid layout css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,32 @@
 <div class="wrapper">
-    <div>sidebar1</div>
-  	<div>Two</div>
-  	<div class="box3">
-    	<p>Three</p>
+  	<div class="grid">
     	<div class="nest">I have</div>
     	<div class="nest">three</div>
     	<div class="nest">boxes</div>
  	</div>
-  	<div>Four</div>
-  	<div>sidebar2</div>
 </div>
+
+<style>
+.wrapper {
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr;
+    grid-auto-rows: 200px;
+}
+  
+.grid {
+    display: grid;
+    position: relative;
+    grid-template-columns: repeat(3, 1fr);
+    grid-column: 2/3;
+}
+  
+.nest {
+    display: flex;
+    justify-content: center;
+    height: 100px;
+}
+  
+.wrapper > div, .nest {
+    border-style: solid;
+}
+</style>
