74bc6de64cb9b2a0399206fea1b36c8b8f76ab30
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-03-24 10:54:41 +1300

    feat: simplified grid layout, less divs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <div class="wrapper">
   	<div class="grid">
-    	<div class="nest">I have</div>
-    	<div class="nest">three</div>
-    	<div class="nest">boxes</div>
+    	<!-- <div class="nest"></div> -->
+    	<!-- <div class="nest"> -->
+            <h1>Testing</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu eros, dapibus accumsan massa at, gravida feugiat nisi. Duis eu aliquet lacus. Vestibulum nec quam faucibus urna tincidunt efficitur. Sed quis est non est ultricies eleifend ut sit amet dui. Cras ultrices risus ut arcu ornare, vel iaculis velit lacinia. Aliquam est turpis, imperdiet quis ante quis, consequat faucibus mi. Proin fermentum nisi fermentum nunc feugiat, ac porta eros vehicula. Suspendisse vitae egestas leo, ullamcorper bibendum diam.</p>
+            <p>Maecenas egestas leo in dapibus molestie. Quisque ac ullamcorper ex, ut elementum mi. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac gravida mauris. Praesent gravida, dolor sit amet iaculis ullamcorper, libero enim rutrum est, ac mollis nunc urna sed felis. Aliquam vitae justo tempor, tincidunt lacus eu, auctor turpis. Nam elementum urna nunc, sed ultricies tellus commodo sed. Donec mi leo, suscipit ut aliquam pulvinar, viverra non nisi. Duis et enim velit.</p>
+        <!-- </div> -->
+    	<!-- <div class="nest"></div> -->
  	</div>
 </div>
 
@@ -10,21 +14,22 @@
 .wrapper {
     display: grid;
     grid-template-columns: 1fr 3fr 1fr;
-    grid-auto-rows: 200px;
+    /* grid-auto-rows: 200px; */
 }
   
 .grid {
-    display: grid;
+    /* display: grid; */
     /* position: relative; */
-    grid-template-columns: repeat(3, 1fr);
+    /* grid-template-columns: repeat(3, 1fr); */
     grid-column: 2/3;
+    
 }
   
-.nest {
+/* .nest {
     display: flex;
     justify-content: center;
     height: 100px;
-}
+} */
   
 /* .wrapper > div, .nest {
     border-style: solid;
