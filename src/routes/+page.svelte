96703fab19ca960a45b7015d8b36d66f41af9b07
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-03-24 11:21:02 +1300

    feat: ninja loaf image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,82 @@
-<div class="wrapper">
-  	<div class="grid">
-    	<!-- <div class="nest"></div> -->
-    	<!-- <div class="nest"> -->
-            <h1>Testing</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu eros, dapibus accumsan massa at, gravida feugiat nisi. Duis eu aliquet lacus. Vestibulum nec quam faucibus urna tincidunt efficitur. Sed quis est non est ultricies eleifend ut sit amet dui. Cras ultrices risus ut arcu ornare, vel iaculis velit lacinia. Aliquam est turpis, imperdiet quis ante quis, consequat faucibus mi. Proin fermentum nisi fermentum nunc feugiat, ac porta eros vehicula. Suspendisse vitae egestas leo, ullamcorper bibendum diam.</p>
-            <p>Maecenas egestas leo in dapibus molestie. Quisque ac ullamcorper ex, ut elementum mi. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac gravida mauris. Praesent gravida, dolor sit amet iaculis ullamcorper, libero enim rutrum est, ac mollis nunc urna sed felis. Aliquam vitae justo tempor, tincidunt lacus eu, auctor turpis. Nam elementum urna nunc, sed ultricies tellus commodo sed. Donec mi leo, suscipit ut aliquam pulvinar, viverra non nisi. Duis et enim velit.</p>
-        <!-- </div> -->
-    	<!-- <div class="nest"></div> -->
- 	</div>
+<script>
+  import loaf from "$lib/loaf.jpg";
+</script>
+
+<div class="grid">
+  <div class="content">
+    <h1>Testing</h1>
+    <section>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu
+        eros, dapibus accumsan massa at, gravida feugiat nisi. Duis eu aliquet
+        lacus. Vestibulum nec quam faucibus urna tincidunt efficitur. Sed quis
+        est non est ultricies eleifend ut sit amet dui. Cras ultrices risus ut
+        arcu ornare, vel iaculis velit lacinia. Aliquam est turpis, imperdiet
+        quis ante quis, consequat faucibus mi. Proin fermentum nisi fermentum
+        nunc feugiat, ac porta eros vehicula. Suspendisse vitae egestas leo,
+        ullamcorper bibendum diam.
+      </p>
+      <p>
+        Maecenas egestas leo in dapibus molestie. Quisque ac ullamcorper ex, ut
+        elementum mi. Aliquam erat volutpat. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Aenean ac gravida mauris. Praesent gravida,
+        dolor sit amet iaculis ullamcorper, libero enim rutrum est, ac mollis
+        nunc urna sed felis. Aliquam vitae justo tempor, tincidunt lacus eu,
+        auctor turpis. Nam elementum urna nunc, sed ultricies tellus commodo
+        sed. Donec mi leo, suscipit ut aliquam pulvinar, viverra non nisi. Duis
+        et enim velit.
+      </p>
+      <img alt="Ninja in Loaf form" src={loaf} />
+      <p>
+        In ac felis et purus tempor luctus sed eget massa. Proin orci magna,
+        efficitur nec ante sit amet, vestibulum egestas ante. Proin et vehicula
+        lacus. Fusce volutpat varius suscipit. Vestibulum malesuada orci vitae
+        sapien tristique blandit. Nam efficitur turpis vel nisl dictum
+        condimentum ac a lectus. Nulla sit amet velit eu sapien porttitor porta
+        vitae at nulla. Praesent sed bibendum orci. Pellentesque tristique
+        scelerisque cursus. Integer non lectus quis enim cursus blandit et in
+        sem. Sed pharetra mi justo, vel pulvinar lorem condimentum eget. Nam
+        elit libero, tincidunt at accumsan in, pharetra eu magna. Nunc
+        sollicitudin ipsum at nunc convallis, vel ultricies lacus lobortis. Duis
+        vulputate neque eget dignissim facilisis.
+      </p>
+      <p>
+        Fusce nisi justo, imperdiet in accumsan non, porta eu tortor. Donec mi
+        justo, accumsan in sem a, porttitor pulvinar quam. Etiam feugiat vel
+        nisl nec vehicula. Nam velit ante, faucibus eget suscipit quis,
+        sollicitudin aliquam ante. Morbi eu mauris id ipsum pellentesque
+        aliquam. Nunc congue quam et urna dictum luctus. Donec libero mauris,
+        fermentum pharetra sagittis at, fringilla id sem. Nullam elementum purus
+        vitae finibus blandit. Mauris et mollis sapien. Sed placerat imperdiet
+        euismod. Pellentesque habitant morbi tristique senectus et netus et
+        malesuada fames ac turpis egestas. Proin fermentum orci a quam
+        pellentesque volutpat. Morbi nec velit in nulla pretium lobortis ac in
+        felis.
+      </p>
+      <p>
+        Maecenas eu tincidunt mauris. Vivamus nec lacinia est. Vivamus at rutrum
+        nulla. Fusce a volutpat enim. Donec hendrerit arcu eu sem tempus
+        interdum eu at odio. Suspendisse accumsan, erat id mattis facilisis,
+        ante dui tristique sapien, nec fringilla metus felis quis arcu.
+        Suspendisse feugiat, libero non ultricies eleifend, ex dui porta est,
+        quis vehicula diam massa id ante. Cras sapien diam, venenatis a sem eu,
+        porta eleifend dui. Nullam ut tellus quam. In quis consequat tellus.
+        Cras vel tristique dui. Cras id dapibus risus. Nulla id ex dui. Mauris
+        tincidunt, arcu ultrices venenatis feugiat, metus ligula varius massa,
+        in faucibus nunc dolor id diam. Quisque in quam quis elit pellentesque
+        aliquam. Morbi lacinia urna risus, ac malesuada mauris pulvinar vel.
+      </p>
+    </section>
+  </div>
 </div>
 
 <style>
-.wrapper {
+  .grid {
     display: grid;
     grid-template-columns: 1fr 3fr 1fr;
-    /* grid-auto-rows: 200px; */
-}
-  
-.grid {
-    /* display: grid; */
-    /* position: relative; */
-    /* grid-template-columns: repeat(3, 1fr); */
+  }
+
+  .content {
     grid-column: 2/3;
-    
-}
-  
-/* .nest {
-    display: flex;
-    justify-content: center;
-    height: 100px;
-} */
-  
-/* .wrapper > div, .nest {
-    border-style: solid;
-} */
+  }
 </style>
