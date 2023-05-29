6d90f63f829a828d4157560ade446736e1615661
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-05-30 09:58:16 +1200

    test: trying opacity on nav bar

--- src/lib/navigation.svelte
+++ src/lib/navigation.svelte
@@ -1,4 +1,15 @@
-<nav>
+<script>
+  // https://svelte.dev/tutorial/inline-styles
+  let backgroundOpacity = 0;
+  // change text colour based on opacity for readability
+  $: colour = backgroundOpacity < 0.6 ? '#000' : '#fff';
+</script>
+
+<!-- try to figure out nav hide on scroll and opacity change -->
+<!-- https://svelte.dev/repl/051cd352ce284d15b55c91c8b30fa32f?version=3.16.7 -->
+<!-- https://stackoverflow.com/questions/67943713/svelte-hide-and-show-nav-on-scroll -->
+
+<nav style="color: {colour}; --opacity: {backgroundOpacity};">
   <ul>
     <li><a href="/">Home</a></li>
     <li><a href="/">2</a></li>
@@ -10,7 +21,7 @@
   nav {
     position: sticky;
     top: 0;
-    background-color: white;
+    background: rgba(255, 255, 255, var(--opacity));
   }
   ul {
     display: flex;
