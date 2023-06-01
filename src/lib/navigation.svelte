f738ba4d1506438bd87a50dc5ae9737f27b4d825
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-06-02 10:42:43 +1200

    feat: buggy way of hiding and showing nav on scroll

--- src/lib/navigation.svelte
+++ src/lib/navigation.svelte
@@ -1,22 +1,39 @@
 <script>
+  import { fly } from 'svelte/transition';
+
   // https://svelte.dev/tutorial/inline-styles
   let backgroundOpacity = 0;
   // change text colour based on opacity for readability
   $: colour = backgroundOpacity < 0.6 ? '#000' : '#fff';
+
+  let y;
+	let newY = [];
+    $: oldY = newY[1];
+    
+    function updateY(){
+        newY.push(y);
+        if(newY.length > 20) {
+            newY.shift();
+        }
+        newY=newY;
+    }
 </script>
 
 <!-- try to figure out nav hide on scroll and opacity change -->
 <!-- https://svelte.dev/repl/051cd352ce284d15b55c91c8b30fa32f?version=3.16.7 -->
 <!-- https://stackoverflow.com/questions/67943713/svelte-hide-and-show-nav-on-scroll -->
 
-<nav style="color: {colour}; --opacity: {backgroundOpacity};">
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/">2</a></li>
-    <li><a href="/">3</a></li>
-  </ul>
-</nav>
+<svelte:window on:scroll={updateY} bind:scrollY={y}/>
 
+{#if oldY > y || y < 100}
+  <nav style="color: {colour}; --opacity: {backgroundOpacity};">
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/">2</a></li>
+      <li><a href="/">3</a></li>
+    </ul>
+  </nav>
+{/if}
 <style>
   nav {
     position: sticky;
