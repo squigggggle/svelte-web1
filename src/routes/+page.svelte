0f836eff2e5de85cd67c5c5ea19bedbafebdcf17
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-06-02 12:02:26 +1200

    fix: replaced duplicate h1 on panel component with h2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,18 @@
 </script>
 <!-- css variable to change direction of panel flow -->
 <Panels --direction="row">
-  <h1 slot="header">
+  <h2 slot="header">
     This cat is so loaf right now
-  </h1>
+  </h2>
   <p slot="paragraph">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcueros, dapibus accumsan massa at, gravida feugiat nisi.
   </p>
   <img slot="image" src="loaf-square.jpg" alt="loaf"/>
 </Panels>
 <Panels --direction="row-reverse">
-  <h1 slot="header">
+  <h2 slot="header">
     test
-  </h1>
+  </h2>
   <p slot="paragraph">
     test 2
   </p>
