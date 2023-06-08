0bc598ce8b92adec882b8c221cb5164fa3ad1433
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-06-09 11:34:25 +1200

    feat: basic styling

--- src/routes/random/+page.svelte
+++ src/routes/random/+page.svelte
@@ -7,12 +7,29 @@
         invalidateAll();                //calls load functions related to this page again
     }
 </script>
+<div>
+    {#if data.status = 200}                 <!--if fetch is successful (code 200) show the image-->
+        <img src="{data.BASE_URL}/{data.cat}" alt= "Random cat">
+    {:else}                                 <!--otherwise show default image-->
+        <img src="loaf-square.jpg" alt="Default cat">
+        <p>Error: {data.status}</p>
+    {/if}
+    
+    <button on:click={rerunLoadFunction}>Update</button>
+</div>
 
-{#if data.status = 200}                 <!--if fetch is successful (code 200) show the image-->
-    <img src="{data.BASE_URL}/{data.cat}" alt= "Random cat">
-{:else}                                 <!--otherwise show default image-->
-    <img src="loaf-square.jpg" alt="Default cat">
-    <p>Error: {data.status}</p>
-{/if}
+<style>
+    div {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    img {
+        width: max-content;
+    }
 
-<button on:click={rerunLoadFunction}>Update</button>
+    button {
+        margin: 1rem 0 0;
+        padding: 1rem;
+    }
+</style>
