2b54d1769a69066f08617e9607f4072b873059fc
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-06-20 11:36:29 +1200

    fix: fade transition fades back out between pages

--- src/lib/navigation.svelte
+++ src/lib/navigation.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { SvelteComponent } from "svelte";
   import { fly, fade } from "svelte/transition";
 
   // https://stackoverflow.com/questions/74776399/how-to-hide-a-div-element-when-scrolling-up-from-any-part-of-the-page-the-svelte
@@ -16,29 +17,28 @@
     newY = newY; //assign to itself so svelte notices change
   }
 
-  // trying to make it so it fades in and out but its not cooperating
-  let visible = false;
-  let hideScroll = "auto";
-  function fadeToBlack(){
-    visible = true;
-    hideScroll = "hidden"
+  //timeout so the transition can fade back in  
+  function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
   }
-  function unFadeToBlack(){
-    visible = false;
-    hideScroll = "auto";
+  
+  //fades div in and fade out after sleep time
+  let visible = false;
+  function toggleDiv(){
+    visible = true;
+    sleep(1000).then(() => visible = false)
   }
 </script>
 
 {#if visible}
-<!-- doesn't un fade idk why -->
-<div transition:fade on:outroend={unFadeToBlack}></div>
+<div transition:fade></div>
 {/if}
 <svelte:window on:scroll={updateY} bind:scrollY={y} />
 {#if oldY > y || y < 67}
   <nav transition:fly>
     <ul>
-      <li><a on:click={fadeToBlack} href="/">Home</a></li>
-      <li><a on:click={fadeToBlack} href="/random">Random</a></li>
+      <li><a on:click={toggleDiv} href="/">Home</a></li>
+      <li><a on:click={toggleDiv} href="/random">Random</a></li>
     </ul>
   </nav>
 {/if}
