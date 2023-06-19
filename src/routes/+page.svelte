28d862f4711fbc03254b08a9033a4cd54748d1ba
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-06-20 09:20:55 +1200

    feat: centre heading and cat descripton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,51 +3,51 @@
 </script>
 
 <div class="heading">
-	<div class="text">
-		<h1>Hello,</h1>
-		<h2>these are my cats</h2>
-		<p>On a website</p>
-	</div>
+    <div class="text">
+        <h1>Hello,</h1>
+        <h2>these are my cats</h2>
+        <p>On a website</p>
+    </div>
 </div>
 <!-- css variable to change direction of panel flow -->
 <Panels --direction="row">
     <h2 slot="header">Ninja</h2>
     <p slot="paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-        arcueros, dapibus accumsan massa at, gravida feugiat nisi.
+        Completely insane.
     </p>
     <img slot="image" src="loaf-square.jpg" alt="Ninja in loaf position" />
 </Panels>
 <Panels --direction="row-reverse">
     <h2 slot="header">Juno</h2>
-    <p slot="paragraph">test 2</p>
+    <p slot="paragraph">
+		Newest cat on the block.
+	</p>
     <img slot="image" src="juno-loaf-square.jpg" alt="Juno in loaf position" />
 </Panels>
 <Panels>
-	<h2 slot="header">Oreo</h2>
-    <p slot="paragraph">test 2</p>
+    <h2 slot="header">Oreo</h2>
+    <p slot="paragraph">
+		Old and wise.
+	</p>
     <img slot="image" src="oreo-square.jpg" alt="Oreo" />
 </Panels>
 
 <style>
-	.heading {
-		display: flex;
-		padding: 6rem;
-		flex-direction: column;
-		
-	}
-	@media (max-width: 768px) {
+    .heading {
+        display: flex;
+        padding: 6rem;
+        flex-direction: column;
+		align-items: center;
+    }
+    @media (max-width: 768px) {
         /* adjusted sizing for mobile-sized viewport */
         div {
             padding: 4.5rem;
         }
     }
-
-	/* see if you can make the text line up with the first panel box text */
-
-	/* .text{
-		display: flex;
-		max-width: 600px;
-		flex-direction: inherit;
-	} */
+    .text {
+        display: flex;
+        max-width: 600px;
+        flex-direction: inherit;
+    }
 </style>
